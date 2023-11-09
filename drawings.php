99b0c83cf131818a681f6e93377db6788ee3fb22
Pramod <pramod52jpr@gmail.com>
2023-11-09 12:32:39 +0530

    added select file system in drawing

--- drawings.php
+++ drawings.php
@@ -86,28 +86,8 @@ if(isset($_GET['del-id']) and !empty($_GET['del-id']) and intval($_GET['del-id']
     }
 }
 ?>
-<?php include "./components/header.php" ?>
-<div class="usersPage">
-    <div class="goback" title="Go Back">
-        <a href="documents.php?proId=<?php echo $_GET['proId'] ?>"><i class="fa-solid fa-arrow-left" style="color: #ffffff;"></i></a>
-    </div>
-    <h2>Drawings</h2>
-    <div class="crudBtn">
-    <?php
-    if($userRow['category']==1){
-        ?>
-            <?php
-            if(isset($_GET['docId']) and isset($_GET['proId'])){
-                $addproject="add-drawing.php?docId=$_GET[docId]&proId=$_GET[proId]";
-            }else{
-                $addproject="#";
-            }
-            ?>
-            <a href="<?php echo $addproject ?>">Add</a>
-            <?php
-        }
-    ?>
-    <?php
+<?php
+if(isset($_POST['checkedData'])){
     $zip = new ZipArchive();
     $docResult=$conn->read("documents","`doc_spec`","`doc_id`=$_GET[docId]");
     $docData=$docResult->fetch_assoc();
@@ -116,82 +96,114 @@ if(isset($_GET['del-id']) and !empty($_GET['del-id']) and intval($_GET['del-id']
     $zipFileName = "./pofiles/drawing-files/$docName-$_SESSION[user_id].zip";
 
     if ($zip->open($zipFileName, ZipArchive::CREATE | ZipArchive::OVERWRITE) === TRUE) {
-        $result=$conn->read("drawings","*","`document_id`=$_GET[docId]");if($result->num_rows>0){
-            while($row=$result->fetch_assoc()){
-                $readHistory=$conn->read("revision_drawings","*","`drawing_id`=$row[draw_id]",null,null,null,"`revision_id` desc");
-                $readHistoryData=$readHistory->fetch_assoc();
-                $zip->addFile("./pofiles/drawing-files/".$readHistoryData['drawing_file'],$readHistoryData['drawing_file']);
-            }
+        $checkedData=$_POST['checkedData'];
+        $over="";
+        foreach($checkedData as $value){
+            $zip->addFile("./pofiles/drawing-files/".$value,$value);
         }
-
         $zip->close();
     }
-    ?>
-
-        <div class="options">
-            <a href="<?php echo $zipFileName ?>" title="DownLoad All Files" style="background-color:green" download><i class="fa-solid fa-download"></i></a>
-            <input type="search" name="search" onkeyup="searchDrawing()" placeholder="Search" id="searchDraw">
-        </div>
-    <?php
     session_abort();
-    ?>
-    </div>
-    <div class="tableContainer">
-        <table cellspacing="0">
-            <thead>
-                <tr>
-                    <th>Number</th>
-                    <th>Assigned Rev.</th>
-                    <th>Title</th>
-                    <th>Last Action Date</th>
-                    <th>File</th>
-                    <th>History</th>
-                    <?php
-                    if($userRow['category']==1){
-                        ?>
-                        <th>Actions</th>
-                        <?php
-                    }
-                    ?>
-                </tr>
-            </thead>
-            <tbody>
+}
+
+?>
+<?php include "./components/header.php" ?>
+<div class="usersPage">
+    <form action="" method="post" style="overflow: visible;margin: 0;background-color: transparent;padding:0;border-radius: 0;">
+        <div class="goback" title="Go Back">
+            <a href="documents.php?proId=<?php echo $_GET['proId'] ?>"><i class="fa-solid fa-arrow-left" style="color: #ffffff;"></i></a>
+        </div>
+        <h2>Drawings</h2>
+        <div class="crudBtn">
+        <?php
+        if($userRow['category']==1){
+            ?>
+                <?php
+                if(isset($_GET['docId']) and isset($_GET['proId'])){
+                    $addproject="add-drawing.php?docId=$_GET[docId]&proId=$_GET[proId]";
+                }else{
+                    $addproject="#";
+                }
+                ?>
+                <a href="<?php echo $addproject ?>">Add</a>
+                <?php
+            }
+        ?>
+        <div class="options">
+            <button type="submit" id="submitBtn" title="Load Selected Files" style="font-size:15px;background-color:rgb(0,150,255);color: white;padding: 10px 20px;font-weight: bold;border-radius: 5px;outline:none;border:none;cursor:pointer;"><i class="fa-solid fa-upload"></i></button>
             <?php
-            $conn=new Conn();
-            $result=$conn->read("drawings","*","`document_id`=$_GET[docId]");
-            if($result->num_rows>0){
-                while($row=$result->fetch_assoc()){
-                    $readHistory=$conn->read("revision_drawings","*","`drawing_id`=$row[draw_id]",null,null,null,"`revision_id` desc");
-                    $readHistoryData=$readHistory->fetch_assoc();
-                    ?>
-                    <tr class="drawingData">
-                        <td><?php echo $row['draw_number'] ?></td>
-                        <td><?php echo $readHistory->num_rows-1 ?></td>
-                        <td><?php echo $row['draw_title'] ?></td>
-                        <td><?php echo $readHistoryData['date'] ?></td>
-                        <td><a href="./pofiles/drawing-files/<?php echo $readHistoryData['drawing_file'] ?>" style="text-decoration: none;" download><i class="fa-solid fa-download"></i></a></td>
-                        <td><a href="history.php?drawId=<?php echo $row['draw_id'] ?>&proId=<?php echo $_GET['proId'] ?>&docId=<?php echo $_GET['docId'] ?>" style="text-decoration: none;"><i class="fa-solid fa-history"></i></a></td>
+            if(!isset($_POST['checkedData'])){
+                $downloadBtnStyle="background-color:grey;pointer-events:none;";
+            }else{
+                $downloadBtnStyle="background-color:green;";
+            }
+            ?>
+            <a href="<?php echo $zipFileName ?>" id="downloadAll" title="Download file" style="<?php echo $downloadBtnStyle ?>" download><i class="fa-solid fa-download"></i></a>
+            <input type="search" name="search" onkeyup="searchDrawing()" placeholder="Search" id="searchDraw" style="width:auto;">
+        </div>
+        <?php
+        session_abort();
+        ?>
+        </div>
+        <div class="tableContainer">
+            <table cellspacing="0">
+                <thead>
+                    <tr>
+                        <th><input type="checkbox" onclick="checkAll()" name="checkedAll" id="checkedAll" style="padding: 0;font-size: auto;border-radius: 0;border: 0;outline: none;width: auto;"> Select</th>
+                        <th>Number</th>
+                        <th>Assigned Rev.</th>
+                        <th>Title</th>
+                        <th>Last Action Date</th>
+                        <th>File</th>
+                        <th>History</th>
                         <?php
                         if($userRow['category']==1){
                             ?>
-                            <td>
-                                <a href="update-drawing.php?id=<?php echo $row['draw_id'] ?>&proId=<?php echo $_GET['proId'] ?>&docId=<?php echo $_GET['docId'] ?>"><i class="fa-solid fa-pen"></i></a>
-                                <a href="?del-id=<?php echo $row['draw_id'] ?>&proId=<?php echo $_GET['proId'] ?>&docId=<?php echo $_GET['docId'] ?>"><i class="fa-solid fa-trash"></i></a>
-                            </td>
+                            <th>Actions</th>
                             <?php
                         }
                         ?>
                     </tr>
+                </thead>
+                <tbody>
+                <?php
+                $conn=new Conn();
+                $result=$conn->read("drawings","*","`document_id`=$_GET[docId]");
+                if($result->num_rows>0){
+                    while($row=$result->fetch_assoc()){
+                        $readHistory=$conn->read("revision_drawings","*","`drawing_id`=$row[draw_id]",null,null,null,"`revision_id` desc");
+                        $readHistoryData=$readHistory->fetch_assoc();
+                        ?>
+                        <tr class="drawingData">
+                            <td><input type="checkbox" onclick="checkedSpecific()" name="checkedData[]" value="<?php echo $readHistoryData['drawing_file'] ?>" class="check" style="padding: 0;font-size: auto;border-radius: 0;border: 0;outline: none;width: auto;"></td>
+                            <td><?php echo $row['draw_number'] ?></td>
+                            <td><?php echo $readHistory->num_rows-1 ?></td>
+                            <td><?php echo $row['draw_title'] ?></td>
+                            <td><?php echo $readHistoryData['date'] ?></td>
+                            <td><a href="./pofiles/drawing-files/<?php echo $readHistoryData['drawing_file'] ?>" style="text-decoration: none;" download><i class="fa-solid fa-download"></i></a></td>
+                            <td><a href="history.php?drawId=<?php echo $row['draw_id'] ?>&proId=<?php echo $_GET['proId'] ?>&docId=<?php echo $_GET['docId'] ?>" style="text-decoration: none;"><i class="fa-solid fa-history"></i></a></td>
+                            <?php
+                            if($userRow['category']==1){
+                                ?>
+                                <td>
+                                    <a href="update-drawing.php?id=<?php echo $row['draw_id'] ?>&proId=<?php echo $_GET['proId'] ?>&docId=<?php echo $_GET['docId'] ?>"><i class="fa-solid fa-pen"></i></a>
+                                    <a href="?del-id=<?php echo $row['draw_id'] ?>&proId=<?php echo $_GET['proId'] ?>&docId=<?php echo $_GET['docId'] ?>"><i class="fa-solid fa-trash"></i></a>
+                                </td>
+                                <?php
+                            }
+                            ?>
+                        </tr>
+                        <?php
+                    }
+                }else{
+                    ?>
+                    <tr align="center"><td colspan="40">No documents exist yet</td></tr>
                     <?php
                 }
-            }else{
                 ?>
-                <tr align="center"><td colspan="40">No documents exist yet</td></tr>
-                <?php
-            }
-            ?>
-            </tbody>
-        </table>
-    </div>
+                </tbody>
+            </table>
+        </div>
+    </form>
 </div>
 <?php include "./components/footer.php" ?>
--- script/dashboard.js
+++ script/dashboard.js
@@ -1,76 +1,137 @@
-$(document).ready(()=>{
+$(document).ready(() => {
     $(".projectList").slideUp(0);
-    $(".dropdown").click(()=>{
+    $(".dropdown").click(() => {
         $(".projectList").slideToggle(200);
     })
 })
 
 // project search
 
-function searchProject(){
-    var searchInput=document.getElementById("search").value;
-    var projectData=document.getElementsByClassName("projectData");
-    for(var i=0;i<projectData.length;i++){
-        if(projectData[i].innerText.toLowerCase().search(searchInput.toLowerCase())!=-1){
-            projectData[i].style.display="";
-        }else{
-            projectData[i].style.display="none";
+function searchProject() {
+    var searchInput = document.getElementById("search").value;
+    var projectData = document.getElementsByClassName("projectData");
+    for (var i = 0; i < projectData.length; i++) {
+        if (projectData[i].innerText.toLowerCase().search(searchInput.toLowerCase()) != -1) {
+            projectData[i].style.display = "";
+        } else {
+            projectData[i].style.display = "none";
         }
     }
 }
 
 // document search
 
-function searchDocument(){
-    var searchInput=document.getElementById("searchDoc").value;
-    var documentData=document.getElementsByClassName("documentData");
-    for(var i=0;i<documentData.length;i++){
-        if(documentData[i].innerText.toLowerCase().search(searchInput.toLowerCase())!=-1){
-            documentData[i].style.display="";
-        }else{
-            documentData[i].style.display="none";
+function searchDocument() {
+    var searchInput = document.getElementById("searchDoc").value;
+    var documentData = document.getElementsByClassName("documentData");
+    for (var i = 0; i < documentData.length; i++) {
+        if (documentData[i].innerText.toLowerCase().search(searchInput.toLowerCase()) != -1) {
+            documentData[i].style.display = "";
+        } else {
+            documentData[i].style.display = "none";
         }
     }
 }
 
 // drawing search
 
-function searchDrawing(){
-    var searchInput=document.getElementById("searchDraw").value;
-    var drawingData=document.getElementsByClassName("drawingData");
-    for(var i=0;i<drawingData.length;i++){
-        if(drawingData[i].innerText.toLowerCase().search(searchInput.toLowerCase())!=-1){
-            drawingData[i].style.display="";
-        }else{
-            drawingData[i].style.display="none";
+function searchDrawing() {
+    var searchInput = document.getElementById("searchDraw").value;
+    var drawingData = document.getElementsByClassName("drawingData");
+    for (var i = 0; i < drawingData.length; i++) {
+        if (drawingData[i].innerText.toLowerCase().search(searchInput.toLowerCase()) != -1) {
+            drawingData[i].style.display = "";
+        } else {
+            drawingData[i].style.display = "none";
         }
     }
 }
 
 // projects in master data
 
-function searchAllProjects(){
-    var searchInput=document.getElementById("searchAllProjects").value;
-    var allProjectData=document.getElementsByClassName("allProjectData");
-    for(var i=0;i<allProjectData.length;i++){
-        if(allProjectData[i].innerText.toLowerCase().search(searchInput.toLowerCase())!=-1){
-            allProjectData[i].style.display="";
-        }else{
-            allProjectData[i].style.display="none";
+function searchAllProjects() {
+    var searchInput = document.getElementById("searchAllProjects").value;
+    var allProjectData = document.getElementsByClassName("allProjectData");
+    for (var i = 0; i < allProjectData.length; i++) {
+        if (allProjectData[i].innerText.toLowerCase().search(searchInput.toLowerCase()) != -1) {
+            allProjectData[i].style.display = "";
+        } else {
+            allProjectData[i].style.display = "none";
         }
     }
 }
 
 // users in master data
 
-function searchAllUsers(){
-    var searchInput=document.getElementById("searchAllUsers").value;
-    var allUsersData=document.getElementsByClassName("allUsersData");
-    for(var i=0;i<allUsersData.length;i++){
-        if(allUsersData[i].innerText.toLowerCase().search(searchInput.toLowerCase())!=-1){
-            allUsersData[i].style.display="";
-        }else{
-            allUsersData[i].style.display="none";
+function searchAllUsers() {
+    var searchInput = document.getElementById("searchAllUsers").value;
+    var allUsersData = document.getElementsByClassName("allUsersData");
+    for (var i = 0; i < allUsersData.length; i++) {
+        if (allUsersData[i].innerText.toLowerCase().search(searchInput.toLowerCase()) != -1) {
+            allUsersData[i].style.display = "";
+        } else {
+            allUsersData[i].style.display = "none";
         }
     }
 }
+
+function checkAll() {
+    var checkedAll = document.getElementById("checkedAll").checked;
+    var allCheckData = document.getElementsByClassName("check");
+    for (var i = 0; i < allCheckData.length; i++) {
+        allCheckData[i].checked = checkedAll;
+    }
+    for (var i = 0; i < allCheckData.length; i++) {
+        if (allCheckData[i].checked == true) {
+            document.getElementById("downloadAll").style.backgroundColor = "grey";
+            document.getElementById("downloadAll").style.pointerEvents = "none";
+            break;
+        } else {
+            if (i == allCheckData.length - 1) {
+                document.getElementById("downloadAll").style.backgroundColor = "green";
+                document.getElementById("downloadAll").style.pointerEvents = "all";
+            }
+        }
+    }
+}
+
+function checkedSpecific() {
+    var checkedAll = document.getElementById("checkedAll");
+    var allCheckData = document.getElementsByClassName("check");
+    for (var i = 0; i < allCheckData.length; i++) {
+        if (allCheckData[i].checked == false) {
+            checkedAll.checked = false;
+            break;
+        } else {
+            checkedAll.checked = true;
+        }
+    }
+    for (var i = 0; i < allCheckData.length; i++) {
+        if (allCheckData[i].checked == true) {
+            document.getElementById("downloadAll").style.backgroundColor = "grey";
+            document.getElementById("downloadAll").style.pointerEvents = "none";
+            break;
+        } else {
+            if (i == allCheckData.length - 1) {
+                document.getElementById("downloadAll").style.backgroundColor = "green";
+                document.getElementById("downloadAll").style.pointerEvents = "all";
+            }
+        }
+    }
+}
+
+document.getElementById("submitBtn").addEventListener("click",
+    function (event) {
+        var allCheckData = document.getElementsByClassName("check");
+        for (var i = 0; i < allCheckData.length; i++) {
+            if (allCheckData[i].checked == true) {
+                break;
+            } else {
+                if (i == allCheckData.length - 1) {
+                    event.preventDefault();
+                    alert("Please Select atleast 1 drawing to Upload");
+                }
+            }
+        }
+    }
+)
